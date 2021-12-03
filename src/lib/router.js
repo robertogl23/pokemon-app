9cb5b2ecab12450772cf74d97129a3326bd38fac
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-12-03 13:52:00 -0600

    Router

---
+++ src/lib/router.js
@@ -0,0 +1,35 @@
+import { PokemonList } from '../components/PokemonList,';
+import { $ } from './dom';
+
+const page = $('#page');
+
+export const getParamUrl = () => window.location.hash.replace(/#/g, '');
+
+export const startRouter = async () => {
+
+
+  const param_value = getParamUrl();
+  if (param_value) {
+    page.setAttribute('name', `pakemon-${param_value}`);
+    page.innerHTML = ``;
+    return;
+  }
+  await PokemonList();
+
+};
+
+export const hashChange = async () => {
+  const param_value = getParamUrl();
+
+  console.log(param_value);
+  if (param_value) {
+    page.setAttribute('name', `pakemon-${param_value}`);
+    $('.grid-pokemon').classList.add('hidden');
+    return;
+  }
+
+  page.setAttribute('name', `pakemon-home`);
+  await PokemonList();
+
+
+};
