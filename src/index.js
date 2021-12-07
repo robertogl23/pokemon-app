20c2cfc36c65660944f96256b628183500641c0a
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-12-07 14:29:33 -0600

    Agregando transition-style dependencia

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import 'transition-style';
 import './css/styles.css';
 import './css/header.css';
 import './css/grid-pokemon.css';
+import './css/path-app.css';
 import { hashChange, startRouter } from './lib/router';
 
 document.addEventListener('DOMContentLoaded', async () => {
